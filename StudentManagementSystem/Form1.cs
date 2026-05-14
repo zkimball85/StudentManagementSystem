6efed25ed1f9992c86d77045e3be7a5c7bc67d42
Zac Kimball <z.kimball85@gmail.com>
2026-05-13 18:49:46 -0700

    Add project files.

---
+++ StudentManagementSystem/Form1.cs
@@ -0,0 +1,15 @@
+namespace StudentManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student stu = new Student();
+        }
+    }
+}
---
+++ StudentManagementSystem/Student.cs
@@ -0,0 +1,33 @@
+﻿
+namespace StudentManagementSystem
+{
+    internal class Student
+    {
+        // Fully implemented C# property
+        private int _studentId;
+
+        public int StudentId
+        {
+            get
+            {
+                return _studentId;
+            }
+            set
+            {
+                _studentId = value;
+            }
+        }
+
+        // Auto-implemented property
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        
+
+    }
+}
